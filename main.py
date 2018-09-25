60509c5318a52bdbccccfa3803ba58f615ab431d
Derek Foundoulis <derek@foundoulis.io>
2018-09-25 16:56:24 -0600

    Added City and Intersection.

--- main.py
+++ main.py
@@ -1,4 +1,32 @@
 
+from queue import Queue
+
+
+class City:
+    """
+    The city
+    """
+
+    the_grid = {} # Intersections to other Intersections
+
+    def __init__(self, height, width):
+        self.width = width
+        self.height = height
+
+class Intersection:
+    """
+
+    """
+    queues = {
+        "nb": Queue(),
+        "sb": Queue(),
+        "eb": Queue(),
+        "wb": Queue()
+    }
+    def __init__(self):
+        pass
+    
+
 def main():
     print("Hello World!")
 
